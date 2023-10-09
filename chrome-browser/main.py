e2478de73924343c419935bdb6b093f7ee528828
Echo <echo-the-coder@tuta.io>
2023-10-09 18:05:22 +0000

    Updates

--- chrome-browser/main.py
+++ chrome-browser/main.py
@@ -4,5 +4,5 @@ os.system("pip install selenium==3.141.0")
 from webbot import Browser
 
 web = Browser()
-web.go_to('https://3kh0.github.io')
+web.go_to('https://duckduckgo.com')
 website = input('Service has audio')
